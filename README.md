7170e38a2adf9f76a5a90e3fa52c121ba743fac8
odiegoduarte <dduartesouza17@gmail.com>
2020-09-03 21:46:06 -0300

    update

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 Olá eu criei esse shell script para facilitar minha vida, sempre que tem um update de kernel do sistema o programa o droidcam pede para que eu faça o update do programa ae eu sempre tinha que ir no site do dev usar o comando para remover depois outro comando para baixar e por fim o comando para instalar o droidcam, ai eu criei um shell script que faz isso com um click.  
 
 <br>
-
+<p align="center">
+  <img width="922" height="455" src=assets/install_droidcam.gif">
+</p>
+<br><br>
 ---
 >*OBS: Esse shellscript é apenas um projeto pessoal de fim de tarde use por sua conta e risco.*
--- droidcam_install.sh
+++ droidcam_install.sh
@@ -2,18 +2,37 @@
 
 # Criado por Diego Duarte 2020
 # https://github.com/odiegoduarte/Install-Droidcam-Unofficial
-#
 # Esse Shell Script foi criado para remover a versão antiga do droidcam baixar a última versão oficial
 # do site e instalar o droidcam.
 
+{
+    for ((i = 0 ; i <= 100 ; i+=7)); do
+        sleep 0.1
+        echo $i
+    done
+} | whiptail --gauge "Iniciando o instalador do Droidcam" 6 50 0
 
+if (whiptail --title " Seja bem-vindo(a)$(whoami) " --yesno "É um simples shell script que foi criado para fazer instalação automatizada do Droidcam.
+No próximo passo irá te pedir a senha de usuário.
+Deseja continuar ?" 10 60) then
+    echo "Iniciando o instalador do Droidcam. . . "
+else
+    echo "Instalação do Droidcam.$6 "
+fi
+
+#Removendo versões anteriores
 sudo /opt/droidcam-uninstall
 
+#Baixando Droidcam
 cd /tmp/
 wget https://files.dev47apps.net/linux/droidcam_latest.zip
 echo "73db3a4c0f52a285b6ac1f8c43d5b4c7 droidcam_latest.zip" | md5sum -c --
 
+#Extraindo e instalando o Droidcam
 unzip droidcam_latest.zip -d droidcam && cd droidcam
 sudo ./install
 
+#Instalando adb para usar o Droidcam via cabo usb.
 sudo apt-get install adb -y
+
+echo "Instalação finalizada !"
